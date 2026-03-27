57ae3c63c4bcd417015853130e420f82f5632c07
d5rcc8rqn4-ship-it <>
2026-03-27 02:13:47 +0000

    Challenge: Inventory Sorting Capstone

--- variables_and_types/challenge_inventory_sorting_capstone/main.py
+++ variables_and_types/challenge_inventory_sorting_capstone/main.py
@@ -11,7 +11,9 @@ category1=categories[0:11]
 category2=categories[13:24]
 print("categories",category1)
 print("categories",category2)
-bubblegum_price=$1.50
-chocolate_price = $2.00
-pasta_price = $5.40
-print(f {candy1} is {bubblegum_price} which is in {category1})
+bubblegum_price=1.50
+chocolate_price = 2.00
+pasta_price = 5.40
+print(f"we have {candy1} for ${bubblegum_price} in the {category1}")
+print(f"we have {candy2} for ${chocolate_price} in the {category1}")
+print(f"we have {dry_goods} for ${pasta_price} in the {category2}")
