c93019a8baf7451a952bf8e460f12ea090b8b877
d5rcc8rqn4-ship-it <>
2026-06-16 22:54:28 +0000

    Challenge: Tuple Operations

--- other_data_types/challenge_tuple_operations/main.py
+++ other_data_types/challenge_tuple_operations/main.py
@@ -9,3 +9,14 @@ if apple_count< 5:
  print("Apples need to be restocked.")
 else:
  print ("Apples are sufficiently stocked.")
+grape_count=shelf.count("grapes")
+if grape_count<2:
+    print("Grapes need to be restocked.")
+else:
+    print("Grapes are sufficiently stocked.")
+if "oranges" in shelf:
+    print("Oranges are at index:", shelf.index("oranges"))
+else:
+    print("Oranges are out of stock")
+    
+
